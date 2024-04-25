11366ff26da1f6343a37cb21c041f6482f63b39d
YaRo <roman.yatsenko@hneu.net>
2024-04-25 19:37:31 +0300

    Add checkout styles

--- orders/forms.py
+++ orders/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 from .models import Order
 
 
@@ -6,3 +7,9 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'city']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({"placeholder": field.label, 'class': 'w-75'})
+            
