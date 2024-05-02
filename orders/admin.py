7b0387d5a095f229a9d4ec863a1e1d073a83c286
roman-yatsenko <roman.yatsenko@hneu.net>
2024-05-02 20:04:37 +0300

    Update 02.05

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,8 @@ import datetime as dt
 
 from django.contrib import admin
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.html import mark_safe
 
 from .models import Order, OrderItem
 
@@ -33,6 +35,11 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
     
     
+def order_detail(obj):
+    url = reverse("orders:admin_order_detail", args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+    
+    
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
@@ -45,6 +52,7 @@ class OrderAdmin(admin.ModelAdmin):
         'paid',
         'created',
         'updated',
+        order_detail,
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
 
 from cart.cart import Cart
 
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
 
 # Create your views here.
 
@@ -25,3 +26,8 @@ def order_create(request):
     else:
         form = OrderCreateForm()
     return render(request, "orders/order/create.html", {'cart': cart, 'form': form})
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, "admin/orders/order/detail.html", {'order': order})
