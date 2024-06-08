02a08cae65ecef93f5b1192cf330d3c71f246ce5
YaRo <roman.yatsenko@hneu.net>
2024-06-08 14:22:20 +0300

    Add i18n & l10n settings

--- orders/forms.py
+++ orders/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from .models import Order
 
@@ -7,6 +8,13 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'city']
+        labels = {
+            'first_name': _("First Name"), 
+            'last_name': _("Last Name"), 
+            'email': "e-mail", 
+            'address': _("Address"), 
+            'city': _("City"),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
