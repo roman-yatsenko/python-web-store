36939fb8ade50466aef0735f5fded57fdf6a9eba
YaRo <roman.yatsenko@hneu.net>
2024-06-15 14:31:15 +0300

    Add url translations

--- core/urls.py
+++ core/urls.py
@@ -19,12 +19,13 @@ from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.utils.translation import gettext_lazy as _
 
 urlpatterns = i18n_patterns(
     path('account/', include('account.urls', namespace='account')),
     path("admin/", admin.site.urls),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('orders/', include('orders.urls', namespace='orders')),
+    path(_('cart/'), include('cart.urls', namespace='cart')),
+    path(_('orders/'), include('orders.urls', namespace='orders')),
     path('coupons/', include('coupons.urls', namespace='coupons')),
     path('i18n/', include('django.conf.urls.i18n')),
     path('translate/', include('rosetta.urls')),
--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
 
-from .views import order_create, admin_order_detail
+from .views import admin_order_detail, order_create
 
 app_name = 'orders'
 
 urlpatterns = [
-    path('create', order_create, name='order_create'),
+    path(_('create'), order_create, name='order_create'),
     path('admin/order/<int:order_id>/', admin_order_detail, name='admin_order_detail'),
 ]
